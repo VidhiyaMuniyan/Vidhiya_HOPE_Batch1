6c5527c7d3d801bf4228571e2935a3240fc0d07e
VidhiyaMuniyan <vidhiyamuniyan@gmail.com>
2026-04-04 21:14:37 +0530

    day_11

--- Day_11/Product.java
+++ Day_11/Product.java
@@ -1,15 +1,19 @@
 package Day_11;
-
 public class Product {
-    private int id;
+     private int id;
     private String name;
     private int price;
-    public Product(){
+    public Product(int id, String name, int price){
         this.id=id;
         this.name=name;
         this.price=price;
     }
     public int getPrice(){
         return this.price;
+    }
+    public void display(){
+        System.out.println("Id: "+this.id);
+        System.out.println("Name: "+this.name);
+        System.out.println("Price:₹"+this.price);
     }
 }
