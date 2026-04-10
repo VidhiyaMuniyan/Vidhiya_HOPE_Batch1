822c0cbd851dbf87adf9407b2bc84f40bb0fa76c
VidhiyaMuniyan <vidhiyamuniyan@gmail.com>
2026-04-10 09:11:12 +0530

    day_3

--- Day_3/MathOperations.java
+++ Day_3/MathOperations.java
@@ -3,77 +3,47 @@ package Day_3;
 import java.util.Scanner;
 
 public class MathOperations {
-    public static void main(String[] args){
-        int a,b;
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the value of a");
-        a= sc.nextInt();
-        System.out.println("Enter the value of b");
+
+        int a, b;
+        System.out.print("Enter value of a: ");
+        a = sc.nextInt();
+
+        System.out.print("Enter value of b: ");
         b = sc.nextInt();
-        //string in switch case
-        String op = sc.nextLine();
-          switch(op){
-            case "+":
-               System.out.println("sum of a+b "+(a+b));
-               break;
-            case "-":
-                 System.out.println("sub of a-b "+(a-b));
-                 break;
-            case "*":
-                 System.out.println("mul of a*b "+(a*b));
-                 break;
-            case "/":
-                 System.out.println("div of a/b "+(a/b));
-                 break;
-            case "%":
-                 System.out.println("mod of a%b "+(a%b));
-                 break;
-            default :
-             System.out.println("invalid operator");
-        }
-       //char in switch case
-      
-       char ch = sc.next().charAt(0);
-          switch(ch){
+
+        System.out.print("Enter operator (+, -, *, /, %): ");
+        char op = sc.next().charAt(0);
+
+        switch (op) {
             case '+':
-               System.out.println("sum of a+b "+(a+b));
-               break;
+                System.out.println("Sum: " + (a + b));
+                break;
+
             case '-':
-                 System.out.println("sub of a-b "+(a-b));
-                 break;
+                System.out.println("Subtraction: " + (a - b));
+                break;
+
             case '*':
-                 System.out.println("mul of a*b "+(a*b));
-                 break;
+                System.out.println("Multiplication: " + (a * b));
+                break;
+
             case '/':
-                 System.out.println("div of a/b "+(a/b));
-                 break;
+                if (b != 0)
+                    System.out.println("Division: " + (a / b));
+                else
+                    System.out.println("Cannot divide by zero");
+                break;
+
             case '%':
-                 System.out.println("mod of a%b "+(a%b));
-                 break;
-            default :
-             System.out.println("invalid operator");
+                System.out.println("Modulus: " + (a % b));
+                break;
+
+            default:
+                System.out.println("Invalid operator");
         }
-        
-       //integer in switch case
-        int n = sc.nextInt();
-          switch(n){
-            case 1:
-               System.out.println("sum of a+b "+(a+b));
-               break;
-            case 2:
-                 System.out.println("sub of a-b "+(a-b));
-                 break;
-            case 3:
-                 System.out.println("mul of a*b "+(a*b));
-                 break;
-            case 4:
-                 System.out.println("div of a/b "+(a/b));
-                 break;
-            case 5:
-                 System.out.println("mod of a%b "+(a%b));
-                 break;
-            default :
-             System.out.println("invalid operator");
-        }
-        }
+
+        sc.close();
+    }
 }
