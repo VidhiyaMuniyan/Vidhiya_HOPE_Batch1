cf8d215bbd8f99a2fe1ed04915e32a5d7fc33801
VidhiyaMuniyan <vidhiyamuniyan@gmail.com>
2026-04-16 06:17:03 +0530

    code base

---
+++ Day_14/FrequencyImplementation.java
@@ -0,0 +1,21 @@
+package Day_14;
+
+import java.util.*;
+public class FrequencyImplementation {
+    public static void main(String[] args) {
+        Map<Character, Integer> frequencyMap = new HashMap<>();
+        String s="aazzzzbbbcc";
+        for (char c : s.toCharArray()) {
+            if (frequencyMap.containsKey(c)) {
+                frequencyMap.put(c, frequencyMap.get(c) + 1);
+            } else {
+                frequencyMap.put(c, 1);
+            }
+        }
+        System.out.println(frequencyMap);
+        for(Map.Entry<Character,Integer> entry:frequencyMap.entrySet()){
+            System.out.println(entry.getKey()+ ":"+entry.getValue());
+        }
+    }
+
+}
---
+++ Day_14/LinkedHashMapImplementation.java
@@ -0,0 +1,14 @@
+package Day_14;
+
+import java.util.LinkedHashMap;
+
+public class LinkedHashMapImplementation {
+    public static void main(String[] args) {
+            LinkedHashMap<String, Integer> linkedHashMap = new LinkedHashMap<>();
+            linkedHashMap.put("One", 1);
+            linkedHashMap.put("Two", 2);
+            linkedHashMap.put("Three", 3);
+            linkedHashMap.put("Four", 4);
+            System.out.println(linkedHashMap);
+    }
+}
